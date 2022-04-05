122397c05f069dc21d190bcf3dc8fe5fd2515da2
Connor Slade <connor@connorcode.com>
2022-04-04 22:01:25 -0400

    🍀 el stuck

--- examples/test.rs
+++ examples/test.rs
@@ -1,16 +1,15 @@
-use std::fs;
-
 use afire::prelude::*;
-use afire_tls::{AfireTls, Identity};
+use afire_tls::AfireTls;
 
 fn main() {
     let mut server = Server::new("localhost", 8080);
 
     server.route(Method::GET, "/", |_| Response::new().text("Ello World"));
     AfireTls::new(
-        Identity::from_pkcs12(&fs::read("./data/localhost.pfx").unwrap(), "Basking2021!").unwrap(),
-    )
-    .attatch(&mut server);
+        include_bytes!("../data/localhost.crt").to_vec(),
+        include_bytes!("../data/localhost.key").to_vec(),
+    );
+    // .attatch(&mut server);
 
     server.start().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,84 @@
-use std::io::{Read, Write};
+use std::io::{Cursor, Read, Write};
+use std::iter;
 use std::sync::Arc;
 
 use afire::{internal::socket_handler::SocketHandler, Server};
-pub use native_tls::Identity;
-use native_tls::TlsAcceptor;
+use rustls::{Certificate, PrivateKey, ServerConfig, ServerConnection};
+use rustls_pemfile::{read_one, Item};
+// pub use rustls::I
 
 pub struct AfireTls {
-    acceptor: Arc<TlsAcceptor>,
+    config: Arc<ServerConfig>,
 }
 
 impl AfireTls {
-    pub fn new(identity: Identity) -> Self {
-        let acceptor = TlsAcceptor::new(identity).unwrap();
-        let acceptor = Arc::new(acceptor);
+    pub fn new(cert: Vec<u8>, key: Vec<u8>) -> Self {
+        // Load Key
+        let mut key = Cursor::new(key);
+        let real_key = match read_one(&mut key).unwrap().unwrap() {
+            Item::RSAKey(key) => key,
+            Item::PKCS8Key(key) => key,
+            Item::ECKey(key) => key,
+            _ => panic!("Invalid Key"),
+        };
 
-        Self { acceptor }
+        // Load Cert
+        let mut cert = Cursor::new(cert);
+        let real_cert = iter::from_fn(|| read_one(&mut cert).transpose())
+            .into_iter()
+            .map(|x| match x.unwrap() {
+                Item::X509Certificate(cert) => cert,
+                _ => panic!("Invalid Certificate"),
+            })
+            .map(|x| Certificate(x))
+            .collect::<Vec<_>>();
+
+        // Build server config
+        let config = ServerConfig::builder()
+            .with_safe_defaults()
+            .with_no_client_auth()
+            .with_single_cert(real_cert, PrivateKey(real_key))
+            .expect("Bad Certificate or Key");
+        let config = Arc::new(config);
+
+        Self { config }
     }
 
     pub fn attatch(self, server: &mut Server) {
-        let ac1 = self.acceptor.clone();
-        let ac2 = self.acceptor.clone();
-        let ac3 = self.acceptor.clone();
-        let ac4 = self.acceptor;
+        let cf1 = self.config.clone();
+        let cf2 = self.config.clone();
+        let cf3 = self.config.clone();
+        let cf4 = self.config;
 
         server.socket_handler = SocketHandler {
-            socket_read: Box::new(move |x, buff| ac1.accept(x).ok()?.read(buff).ok()),
-            socket_read_exact: Box::new(move |x, buff| ac2.accept(x).ok()?.read_exact(buff).ok()),
-            socket_flush: Box::new(move |x| ac3.accept(x).ok()?.flush().ok()),
-            socket_write: Box::new(move |x, y| ac4.accept(x).unwrap().write_all(y).ok()),
+            socket_read: Box::new(move |x, buff| {
+                let mut conn = ServerConnection::new(cf1.to_owned()).ok()?;
+                conn.read_tls(x).ok()?;
+                let out = conn.reader().read(buff).ok();
+                conn.process_new_packets().ok();
+                out
+            }),
+            socket_read_exact: Box::new(move |x, buff| {
+                let mut conn = ServerConnection::new(cf2.to_owned()).ok()?;
+                conn.read_tls(x).ok()?;
+                let out = conn.reader().read_exact(buff).ok();
+                conn.process_new_packets().ok();
+                out
+            }),
+            socket_flush: Box::new(move |x| {
+                let mut conn = ServerConnection::new(cf3.to_owned()).ok()?;
+                conn.write_tls(x).ok()?;
+                let out = conn.writer().flush().ok();
+                conn.process_new_packets().ok();
+                out
+            }),
+            socket_write: Box::new(move |x, y| {
+                let mut conn = ServerConnection::new(cf4.to_owned()).ok()?;
+                conn.write_tls(x).ok()?;
+                let out = conn.writer().write_all(y).ok();
+                conn.process_new_packets().ok();
+                out
+            }),
         };
     }
 }
